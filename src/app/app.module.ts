f77f686e487b58650826098bb212827a182edb28
GELMER OTTONIEL MOLINA <gelmer.ottoniel.molina@ericsson.com>
2020-03-04 20:13:47 -0600

    Creacion de componente para grafica de arbol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxEchartsModule } from 'ngx-echarts';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,18 +10,22 @@ import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GrafoComponent } from './grafo/grafo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GrafoComponent
   ],
   imports: [
     BrowserModule,
+    NgxEchartsModule,
     MatSidenavModule,
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    MatButtonModule,
     BrowserAnimationsModule
   ],
   exports: [
